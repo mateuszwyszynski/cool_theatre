7faf159c8f3c4ac5430ee6c0bfbdafaf5f5ebbbd
Mateusz <mat.wyszynski@gmail.com>
2018-10-11 11:06:37 +0200

    Create tests for adding connection genes to a genome.

--- src/test/scala/Genomes.scala
+++ src/test/scala/Genomes.scala
@@ -238,4 +238,150 @@ class Genomes extends Matchers
       genome.addNodeAtConnection(1, 3, 0.4) should equal(resultGenome)
     }
   }
+
+  "Adding a new connection gene to a genome" should {
+    "add new connection" in {
+      val connections: List[ConnectionGene] =
+        List(ConnectionGene(1, 3, 1.0, enabled = true), ConnectionGene(2, 3, 1.0, enabled = false),
+          ConnectionGene(2, 4, 1.0, enabled = true), ConnectionGene(4, 3, 1.0, enabled = true))
+
+      val nodes: Map[Int, NodeGene] =
+        Map((1, SensorGene()), (2, SensorGene()), (3, OutputGene(0.7)), (4, HiddenGene(0.4)))
+
+      val genome: Genome = Genome(nodes, connections)
+
+      val resultConns: List[ConnectionGene] =
+        List(ConnectionGene(1, 3, 1.0, enabled = true), ConnectionGene(2, 3, 1.0, enabled = false),
+          ConnectionGene(2, 4, 1.0, enabled = true), ConnectionGene(4, 3, 1.0, enabled = true),
+          ConnectionGene(1, 4, 0.5, enabled = true))
+
+      val resultNodes: Map[Int, NodeGene] =
+        Map((1, SensorGene()), (2, SensorGene()), (3, OutputGene(0.7)), (4, HiddenGene(0.4)))
+
+      val resultGenome: Genome = Genome(resultNodes, resultConns)
+
+      genome.addConnectionBetween(1, 4, 0.5) should equal(resultGenome)
+    }
+
+    "enable and change weight of a disabled connection with the same input and output" in {
+      val connections: List[ConnectionGene] =
+        List(ConnectionGene(1, 3, 1.0, enabled = true), ConnectionGene(2, 3, 1.0, enabled = false),
+          ConnectionGene(2, 4, 1.0, enabled = true), ConnectionGene(4, 3, 1.0, enabled = true))
+
+      val nodes: Map[Int, NodeGene] =
+        Map((1, SensorGene()), (2, SensorGene()), (3, OutputGene(0.7)), (4, HiddenGene(0.4)))
+
+      val genome: Genome = Genome(nodes, connections)
+
+      val resultConns: List[ConnectionGene] =
+        List(ConnectionGene(1, 3, 1.0, enabled = true), ConnectionGene(2, 3, 0.5, enabled = true),
+          ConnectionGene(2, 4, 1.0, enabled = true), ConnectionGene(4, 3, 1.0, enabled = true))
+
+      val resultNodes: Map[Int, NodeGene] =
+        Map((1, SensorGene()), (2, SensorGene()), (3, OutputGene(0.7)), (4, HiddenGene(0.4)))
+
+      val resultGenome: Genome = Genome(resultNodes, resultConns)
+
+      genome.addConnectionBetween(2, 3, 0.5) should equal(resultGenome)
+    }
+
+    "enable and change weight of only the first disabled connection with the same input and output" in {
+      val connections: List[ConnectionGene] =
+        List(ConnectionGene(1, 3, 1.0, enabled = true), ConnectionGene(2, 3, 1.0, enabled = false),
+          ConnectionGene(2, 4, 1.0, enabled = true), ConnectionGene(4, 3, 1.0, enabled = true),
+          ConnectionGene(2, 3, 0.7, enabled = false))
+
+      val nodes: Map[Int, NodeGene] =
+        Map((1, SensorGene()), (2, SensorGene()), (3, OutputGene(0.7)), (4, HiddenGene(0.4)))
+
+      val genome: Genome = Genome(nodes, connections)
+
+      val resultConns: List[ConnectionGene] =
+        List(ConnectionGene(1, 3, 1.0, enabled = true), ConnectionGene(2, 3, 0.5, enabled = true),
+          ConnectionGene(2, 4, 1.0, enabled = true), ConnectionGene(4, 3, 1.0, enabled = true),
+          ConnectionGene(2, 3, 0.7, enabled = false))
+
+      val resultNodes: Map[Int, NodeGene] =
+        Map((1, SensorGene()), (2, SensorGene()), (3, OutputGene(0.7)), (4, HiddenGene(0.4)))
+
+      val resultGenome: Genome = Genome(resultNodes, resultConns)
+
+      genome.addConnectionBetween(2, 3, 0.5) should equal(resultGenome)
+    }
+
+    "enable and change weight of the first disabled connection with the same input and output and" +
+      "do not change any enabled connections with the same input and output" in {
+      val connections: List[ConnectionGene] =
+        List(ConnectionGene(1, 3, 1.0, enabled = true), ConnectionGene(2, 3, 1.0, enabled = false),
+          ConnectionGene(2, 4, 1.0, enabled = true), ConnectionGene(4, 3, 1.0, enabled = true),
+          ConnectionGene(2, 3, 0.7, enabled = true), ConnectionGene(2, 3, 0.6, enabled = true))
+
+      val nodes: Map[Int, NodeGene] =
+        Map((1, SensorGene()), (2, SensorGene()), (3, OutputGene(0.7)), (4, HiddenGene(0.4)))
+
+      val genome: Genome = Genome(nodes, connections)
+
+      val resultConns: List[ConnectionGene] =
+        List(ConnectionGene(1, 3, 1.0, enabled = true), ConnectionGene(2, 3, 0.5, enabled = true),
+          ConnectionGene(2, 4, 1.0, enabled = true), ConnectionGene(4, 3, 1.0, enabled = true),
+          ConnectionGene(2, 3, 0.7, enabled = true), ConnectionGene(2, 3, 0.6, enabled = true))
+
+      val resultNodes: Map[Int, NodeGene] =
+        Map((1, SensorGene()), (2, SensorGene()), (3, OutputGene(0.7)), (4, HiddenGene(0.4)))
+
+      val resultGenome: Genome = Genome(resultNodes, resultConns)
+
+      genome.addConnectionBetween(2, 3, 0.5) should equal(resultGenome)
+    }
+
+    "throw a WrongConnectionGene exception" when {
+      "specified input is wrong" in {
+        val connections: List[ConnectionGene] =
+          List(ConnectionGene(1, 3, 1.0, enabled = true), ConnectionGene(2, 3, 1.0, enabled = false),
+            ConnectionGene(2, 4, 1.0, enabled = true), ConnectionGene(4, 3, 1.0, enabled = true))
+
+        val nodes: Map[Int, NodeGene] =
+          Map((1, SensorGene()), (2, SensorGene()), (3, OutputGene(0.7)), (4, HiddenGene(0.4)))
+
+        val genome: Genome = Genome(nodes, connections)
+
+        val thrown =
+          the [WrongConnectionGene] thrownBy genome.addConnectionBetween(5, 3, 0.5)
+
+        thrown.getMessage should equal("No node found within the genome for the specified input.")
+      }
+
+      "specified output is wrong" in {
+        val connections: List[ConnectionGene] =
+          List(ConnectionGene(1, 3, 1.0, enabled = true), ConnectionGene(2, 3, 1.0, enabled = false),
+            ConnectionGene(2, 4, 1.0, enabled = true), ConnectionGene(4, 3, 1.0, enabled = true))
+
+        val nodes: Map[Int, NodeGene] =
+          Map((1, SensorGene()), (2, SensorGene()), (3, OutputGene(0.7)), (4, HiddenGene(0.4)))
+
+        val genome: Genome = Genome(nodes, connections)
+
+        val thrown =
+          the [WrongConnectionGene] thrownBy genome.addConnectionBetween(2, 8, 0.5)
+
+        thrown.getMessage should equal("No node found within the genome for the specified output.")
+      }
+
+      "both specified input and output are wrong" in {
+        val connections: List[ConnectionGene] =
+          List(ConnectionGene(1, 3, 1.0, enabled = true), ConnectionGene(2, 3, 1.0, enabled = false),
+            ConnectionGene(2, 4, 1.0, enabled = true), ConnectionGene(4, 3, 1.0, enabled = true))
+
+        val nodes: Map[Int, NodeGene] =
+          Map((1, SensorGene()), (2, SensorGene()), (3, OutputGene(0.7)), (4, HiddenGene(0.4)))
+
+        val genome: Genome = Genome(nodes, connections)
+
+        val thrown =
+          the [WrongConnectionGene] thrownBy genome.addConnectionBetween(5, 8, 0.5)
+
+        thrown.getMessage should equal("No nodes found within the genome for both input and output.")
+      }
+    }
+  }
 }
